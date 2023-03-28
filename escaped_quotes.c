91fee07123b69a79b2e719e6a9096f81aeda5857
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-03-28 15:38:06 -0700

    Edited-betty style

--- escaped_quotes.c
+++ escaped_quotes.c
@@ -4,25 +4,25 @@
  * print_bslash - write back-slash to stdout
  * Return: number of printed chars 1
 */
-int print_bslash()
+int print_bslash(void)
 {
-        return (_putchar(92));
+	return (_putchar(92));
 }
 
 /**
  * print_sq - write single quote to stdout
  * Return: number of printed char
 */
-int print_sq()
+int print_sq(void)
 {
-        return (_putchar(44));
+	return (_putchar(44));
 }
 
 /**
  * print_dq - write double quote to stdout
  * Return: number of printed char
 */
-int print_dq()
+int print_dq(void)
 {
-        return(_putchar(34));
+	return (_putchar(34));
 }
